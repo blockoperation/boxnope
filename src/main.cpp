2114d7589e894afb0ffe18248ff604005c17000f
blockoperation <blockoperation@users.noreply.github.com>
2016-11-30 21:59:17 +0000

    Version bump

--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@ int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
     QApplication::setApplicationName(QSL("boxnope"));
-    QApplication::setApplicationVersion(QSL("0.1"));
+    QApplication::setApplicationVersion(QSL("0.2"));
     QObject::connect(&app, &QApplication::lastWindowClosed, [&app]() { app.quit(); });
 
     QCommandLineParser arg_parser;
